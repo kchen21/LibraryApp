0c7513782bb64972d231b15a276c8c1e08334477
Kenneth Chen <kennethchendbgt@gmail.com>
2017-07-27 19:07:25 -0400

    Create saveInvitation action

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,9 +2,21 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
+  headerMessage: 'Coming Soon',
   emailAddress: '',
+  responseMessage: '',
 
   isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
-  isDisabled: Ember.computed.not('isValid')
+  isDisabled: Ember.computed.not('isValid'),
+
+  actions: {
+
+    saveInvitation() {
+      alert(`Saving of the following email address is in progress: ${this.get('emailAddress')}`);
+      this.set('responseMessage', `Thank you! We've just saved your email address" ${this.get('emailAddress')}`);
+      this.set('emailAddress', '');
+    }
+    
+  }
 
 });
