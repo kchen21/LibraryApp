006bc1462978543a6eaf9f2330c4b503bff1e4b6
Kenneth Chen <kennethchendbgt@gmail.com>
2017-08-01 23:18:21 -0400

    Refactor invitation section, moving validation into model and actions into route handler

---
+++ app/routes/index.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+
+  model() {
+    return this.store.createRecord('invitation');
+  },
+
+  actions: {
+
+    saveInvitation(newInvitation) {
+      newInvitation.save().then(() => this.controller.set('responseMessage', true));
+    },
+
+    willTransition() {
+      this.controller.get('model').rollbackAttributes();
+      this.controller.set('responseMessage', false);
+    }
+
+  }
+
+});
