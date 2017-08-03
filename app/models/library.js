8503aeafea3de0a7b44eb956a5a2cf4cd08b3851
Kenneth Chen <kennethchendbgt@gmail.com>
2017-08-03 19:05:49 -0400

    Set up methods for creating library, book, and author data using Faker

--- app/models/library.js
+++ app/models/library.js
@@ -1,14 +1,28 @@
 import DS from 'ember-data';
 import Ember from 'ember';
+import Faker from 'faker';
 
 export default DS.Model.extend({
 
   name: DS.attr('string'),
   address: DS.attr('string'),
   phone: DS.attr('string'),
-  
+
   books: DS.hasMany('book'),
 
-  isValid: Ember.computed.notEmpty('name')
+  isValid: Ember.computed.notEmpty('name'),
+
+  randomize() {
+    this.set('name', Faker.company.companyName() + ' Library');
+    this.set('address', this._fullAddress());
+    this.set('phone', Faker.phone.phoneNumber());
+
+    // For chaining
+    return this;
+  },
+
+  _fullAddress() {
+    return `${Faker.address.streetAddress()}, ${Faker.address.city()}`;
+  }
 
 });
