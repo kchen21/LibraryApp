3ffa394060b3309cc8569463cdd4c91c2f0cc555
Kenneth Chen <kennethchendbgt@gmail.com>
2017-08-04 13:50:52 -0400

    Update authors route handler and template to support name-editing

--- app/routes/authors.js
+++ app/routes/authors.js
@@ -4,6 +4,28 @@ export default Ember.Route.extend({
 
   model() {
     return this.store.findAll('author');
+  },
+
+  actions: {
+
+    editAuthor(author) {
+      author.set('isEditing', true);
+    },
+
+    cancelAuthorEdit(author) {
+      author.set('isEditing', false);
+      author.rollbackAttributes();
+    },
+
+    saveAuthor(author) {
+      if (author.get('isInvalid')) {
+        return;
+      }
+
+      author.set('isEditing', false);
+      author.save();
+    }
+
   }
 
 });
